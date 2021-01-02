0173773221518ecd6d38357c75bef57ba048dda0
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-02 18:44:03 +0200

    Add a test document

---
+++ index.js
@@ -0,0 +1,28 @@
+//Prototype script
+
+const highlight = require("highlight.js");
+
+const MarkdownIt = require("markdown-it");
+const md = new MarkdownIt({
+    highlight: (str, lang) => {
+        if (lang && highlight.getLanguage(lang)) {
+            try {
+                return highlight.highlight(lang, str).value;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        return ''; // use external default escaping.
+    }
+});
+
+const fs = require("fs");
+
+const testMarkdown = fs.readFileSync("source/test.md", "utf-8");
+const testHtml = md.render(testMarkdown);
+
+const templateHtml = fs.readFileSync("public/index.html", "utf-8");
+const testStatic = templateHtml.replace("Hello main content!", testHtml);
+
+fs.writeFileSync("public/test.html", testStatic);
---
+++ source/test.md
@@ -0,0 +1,45 @@
+# Heading 1
+
+## Heading 2
+
+### Heading 3
+
+#### Heading 4
+
+##### Heading 5
+
+###### Heading 6
+
+Hello markdown!
+
+Line 1
+Line 2 same paragraph
+
+**this is bold**
+
+*this is italic*
+
+__this is underlined__
+
+~~This is stricked through~~
+
+[This links to google](https://www.google.com/)
+
+[This links to index.html](/index.html)
+
+![this should be an image](img/logo-icon.svg)
+
+> this is a quote
+
+This is an `in-line codeblock`
+
+```
+this is a
+multiline codeblock
+```
+
+---
+
+```lua
+print("This is some Lua code!");
+```
