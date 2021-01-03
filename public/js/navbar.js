ddfda45317a2a451afd9939c34ff0d3d51cdd56d
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-03 21:50:51 +0200

    Remove navbar.js, it's not needed

--- public/js/navbar.js
+++
@@ -1,4 +0,0 @@
-const navbarElement = document.querySelector(".navbar");
-const toggleButton = document.querySelector(".navbar .mobile-toggle");
-
-toggleButton.onclick = () => navbarElement.classList.toggle("active");
