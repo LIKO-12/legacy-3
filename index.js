91a63643b5dfb52b2ebbad5274f985174f484866
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-04 22:06:13 +0200

    Add the documents rendering

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const Styles = require("./src/styles");
 
 Styles.attemptToCompileStandardDirectory();
 Static.copyStandardDirectory();
+Templates.loadStandardTemplates();
+Templates.loadStandardPartials();
+Renderer.renderStandardDirectory();
 
 // ------------------------------ Watching directories ------------------------------ //
 
