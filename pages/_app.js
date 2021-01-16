90d2cdd8e4812d83d1f6fb03318aab0c3a8c0449
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-16 15:57:29 +0200

    Include the global styles on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../node_modules/normalize.css/normalize.css";
+import "../styles/global.scss";
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
