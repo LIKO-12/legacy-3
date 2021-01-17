237e1bf36eb5d952662ef86a3f83788e4909c1cb
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-17 11:08:26 +0200

    Remove "Hello side panel!"

--- components/sidepanel.js
+++ components/sidepanel.js
@@ -2,6 +2,6 @@ import styles from './sidepanel.module.scss';
 
 export default function SidePanel() {
     return (<nav className={styles.side_panel}>
-        Hello side panel!
+        
     </nav>);
 }
