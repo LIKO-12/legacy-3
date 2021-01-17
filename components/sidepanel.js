48f93467b3c83acca83551f70b0a6d1a56abdcf0
Rami Sabbagh <ramilego4game@gmail.com>
2021-01-17 11:16:14 +0200

    Use the sidepanel to display debug data

--- components/sidepanel.js
+++ components/sidepanel.js
@@ -1,7 +1,16 @@
 import styles from './sidepanel.module.scss';
+import { useRouter } from 'next/router';
 
 export default function SidePanel() {
+    const router = useRouter();
+
     return (<nav className={styles.side_panel}>
-        
+        <ul>
+            <li><b>Current route: {' '}</b> <code>{router.pathname}</code></li><br />
+            <li><b>Router query: {' '}</b> <code>{toString(router.query)}</code></li><br />
+            <li><b>As path: {' '}</b> <code>{router.asPath}</code></li><br />
+        </ul>
+
+
     </nav>);
 }
