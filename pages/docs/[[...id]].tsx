a94496966d2d5e2ab1186be1c0450efbb3e6ac8d
Rami Sabbagh <ramilego4game@gmail.com>
2021-02-01 18:17:49 +0200

    Fix typescript warnings/errors

--- pages/docs/[[...id]].tsx
+++ pages/docs/[[...id]].tsx
@@ -1,8 +1,9 @@
 import { GetStaticProps, GetStaticPaths } from 'next';
-import { getDocumentData, getAllDocumentsPaths } from '../../lib/docs';
+import { getDocumentData, getAllDocumentsPaths, DocumentData } from '../../lib/docs';
 import Document from '../../components/document';
 
-export default function DocumentPage({ documentData: { title, contentHtml } }) {
+type DocumentPageProps = { documentData: DocumentData };
+const DocumentPage: React.FC<DocumentPageProps> = ({ documentData: { title, contentHtml } }) => {
     return (
         <Document>
             {title ? <h1>{title}</h1> : null}
@@ -10,9 +11,10 @@ export default function DocumentPage({ documentData: { title, contentHtml } }) {
         </Document>
     );
 }
+export default DocumentPage;
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const id = params.id instanceof Array ? params.id.join("/") : (params.id || "");
+    const id = params?.id instanceof Array ? params.id.join("/") : (params?.id || "");
     const documentData = await getDocumentData(id);
 
     return { props: { documentData } }
