d7f978762c0b10bce5bbc1e03155cbd4f6b18243
Rami Sabbagh <ramilego4game@gmail.com>
2021-02-01 16:20:30 +0200

    Declare type interface for markdown-it-replace-link

---
+++ @types/markdown-it-replace-link.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for markdown-it-replace-link
+// Project: LIKO-12's Website
+// Definitions by: Rami Sabbagh <https://rami-sabbagh.github.io>
+
+declare module "markdown-it-replace-link" {
+    declare function mdlink(md: any): void;
+    export = mdlink;
+}
